f171124d4ed58e5bd7a1447340e533b6263ba4a2
Etomchukwu Oguejiofor <etoogueji@gmail.com>
2023-07-05 22:10:21 +0100

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,6 +4,7 @@ def max_integer(my_list=[]):
         return
         
     i = len(my_list) - 1
+    k = 1
 
     while i > 1:
         j = 0
@@ -14,4 +15,4 @@ def max_integer(my_list=[]):
                 my_list[j + 1] = temp
             j += 1
         i -= 1
-    return my_list[len(my_list) - 1]
+    return my_list[k]

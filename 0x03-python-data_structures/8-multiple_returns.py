559784fded2a6daab9a0e500f773efd7551a125b
EtoOgueji <etoogueji@gmail.com>
2023-07-05 13:05:28 -0700

    returns tuple(len(string), string[0])

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     t = ()
     if len(sentence) == 0:
-        sentence[0] =  None
+        sentence[0] = None
 
-    my_tuple = len(sentence), sentence[0]
+     t = len(sentence), sentence[0])
     return t
